0268c981b255fa9fb5187122aa2607879465c255
GehanFonseka <gehanfonseka@gmail.com>
2024-12-15 21:23:11 +0530

    adding about page

--- about.php
+++ about.php
@@ -1,98 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require "includes/header.php"; ?>
+<?php require "config/config.php"; ?>
 
-  <head>
+<?php
+   if (isset($_GET['id'])) {
+      $id = $_GET['id'];
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      $country = $conn->query("SELECT * FROM countries WHERE id='$id'");
+      $country->execute();
 
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+      $singleCountry = $country->fetch(PDO::FETCH_OBJ);
 
-    <title>WoOx Travel - About Us</title>
+      // Check if country data was found
+      if ($singleCountry) {
+         $countryName = $singleCountry->name;
+         $countryDescription = $singleCountry->description;
+      } else {
+         $countryName = "Country Not Found";
+         $countryDescription = "Sorry, the country information could not be found.";
+      }
+   } else {
+      $countryName = "No Country Selected";
+      $countryDescription = "Please select a country to explore.";
+   }
+?>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-woox-travel.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
-<!--
-
-TemplateMo 580 Woox Travel
-
-https://templatemo.com/tm-580-woox-travel
-
--->
-  </head>
-
-<body>
-
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div>
-  <!-- ***** Preloader End ***** -->
-
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="index.html" class="logo">
-                        <img src="assets/images/logo.png" alt="">
-                    </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
-                    <ul class="nav">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="deals.html">Deals</a></li>
-                      
-                        <li><a href="login.html">Login</a></li>
-                        <li><a href="register.html">Register</a></li>
-                    </ul>   
-                    <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
-                    <!-- ***** Menu End ***** -->
-                </nav>
-            </div>
-        </div>
-    </div>
-  </header>
-  <!-- ***** Header Area End ***** -->
-
-  <!-- ***** Main Banner Area Start ***** -->
-  <div class="about-main-content">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="content">
-            <div class="blur-bg"></div>
-            <h4>EXPLORE OUR COUNTRY</h4>
-            <div class="line-dec"></div>
-            <h2>Welcome To Caribbean</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uttersi labore et dolore magna aliqua is ipsum suspendisse ultrices gravida</p>
-            <div class="main-button">
-            </div>
+<!-- ***** Main Banner Area Start ***** -->
+<div class="about-main-content">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="content">
+          <div class="blur-bg"></div>
+          <h4>EXPLORE OUR COUNTRY</h4>
+          <div class="line-dec"></div>
+          <h2>Welcome To <?php echo htmlspecialchars($countryName); ?></h2>
+          <p><?php echo htmlspecialchars($countryDescription); ?></p>
+          <div class="main-button">
           </div>
         </div>
       </div>
     </div>
   </div>
-  <!-- ***** Main Banner Area End ***** -->
-  
+</div>
   <div class="cities-town">
     <div class="container">
       <div class="row">
@@ -331,39 +280,4 @@ https://templatemo.com/tm-580-woox-travel
     </div>
   </div>
 
-
-  
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <p>Copyright © 2036 <a href="#">WoOx Travel</a> Company. All rights reserved. 
-          <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a> Distribution: <a href="https://themewagon.com target="_blank" >ThemeWagon</a></p>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/wow.js"></script>
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
-
-  <script>
-    $(".option").click(function(){
-      $(".option").removeClass("active");
-      $(this).addClass("active"); 
-    });
-  </script>
-
-  </body>
-
-</html>
+<?php require "includes/footer.php"; ?>
